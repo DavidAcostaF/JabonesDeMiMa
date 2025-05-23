c3a7909eec64491bd51999cf20eb73e5c6a3a755
AlanManjarrez <160210523+AlanManjarrez@users.noreply.github.com>
2025-05-23 01:08:47 -0700

    Add success messages for product creation and update in views

--- apps/products/views.py
+++ apps/products/views.py
@@ -10,6 +10,8 @@ from django.views.generic import UpdateView
 from django.forms import inlineformset_factory
 from django.views.generic import DeleteView
 from django.views.generic import DetailView
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
 
 class ProductCreateView(CreateView):
     model = Product
@@ -44,6 +46,7 @@ class ProductCreateView(CreateView):
                     ingredient = ingredient_form.save(commit=False)
                     ingredient.product = product
                     ingredient.save()
+                    messages.success(request, "Producto creado correctamente")  
             return redirect(self.success_url)
 
         return render(request, self.template_name, {
@@ -84,6 +87,7 @@ class ProductUpdateView(UpdateView):
         if formset.is_valid():
             form.formset = formset
             self.object = form.save()
+            messages.success(self.request, "Producto actualizado correctamente")
             return redirect(self.success_url)
         else:
             return self.render_to_response(self.get_context_data(form=form))
